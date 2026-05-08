c17bada4bf528380ebe03113078e764b25545d19
Suvasis76 <suvasisjanacool1000@gmail.com>
2026-05-08 10:23:12 +0530

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -63,7 +63,7 @@ const Footer = () => {
         <div className="flex gap-6">
           <Link 
             className="text-slate-500 hover:text-white transition-colors" 
-            href="https://www.facebook.com/people/SKC-Infotech/100063916843411/#"
+            href="https://www.facebook.com/profile.php?id=61556555216851"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Facebook"
@@ -81,10 +81,19 @@ const Footer = () => {
           </Link>
           <Link 
             className="text-slate-500 hover:text-white transition-colors" 
-            href="https://www.linkedin.com/company/skcinfotech/?originalSubdomain=in"
+            href="https://www.linkedin.com/in/sushanta-chakraborty-ba1882392"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="LinkedIn"
+          >
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
+          </Link>
+          <Link 
+            className="text-slate-500 hover:text-white transition-colors" 
+            href="https://www.instagram.com/skc_infotech?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
           >
             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
           </Link>
